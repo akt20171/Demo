7e13e4fea55f0158bd8645927b52243502d2cdce
akt20171 <elearningakt@gmail.com>
2026-02-22 01:11:17 -0500

    feature: add letter grade output

--- calculator.py
+++ calculator.py
@@ -1,6 +1,19 @@
 def get_average(grades):
     return sum(grades) / len(grades)
 
+def get_letter_grade(average):
+    if average >= 90:
+        return "A"
+    elif average >= 80:
+        return "B"
+    elif average >= 70:
+        return "C"
+    elif average >= 60:
+        return "D"
+    else:
+        return "F"
+
 def print_result(name, grades):
     avg = get_average(grades)
     print(name + " average: " + str(avg))
+    print(name + " letter grade: " + get_letter_grade(avg))
